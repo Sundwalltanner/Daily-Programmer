97dafa718337da9bf53f1d4e9e199105919ced6c
Tanner Sundwall <sundwalltanner@gmail.com>
2020-03-13 05:34:00 -0700

    Optimization and comments

--- 03_2020/necklace_matching.cpp
+++ 03_2020/necklace_matching.cpp
@@ -19,6 +19,8 @@ bool same_necklace(const string, const string);
 
 int main()
 {
+    // If one of the asserts below fails, a message is written to the terminal
+    // notifying us of which assert failed, and the program is terminated.
     assert(same_necklace("nicole", "icolen"));
     assert(same_necklace("nicole", "lenico"));
     assert(!same_necklace("nicole", "coneli"));
@@ -31,44 +33,50 @@ int main()
     assert(!same_necklace("x", ""));
     assert(same_necklace("", ""));
 
+    // If we hit this, none of the asserts above failed, so we've succeeded.
     cout << "Success!" << endl;
 
     return 0;
 }
 
+// Checks two strings to see if they could possibly match if all the letters from each string
+// were placed on two separate necklaces.
 bool same_necklace(const string str_1, const string str_2)
 {
     int str_length = str_1.length();
 
+    // If the two strings aren't the same length, they can't be equal.
     if (str_length != str_2.length()) return false;
+
+    // If both strings are empty, they're equal.
     if (str_length == 0) return true;
-    if (str_length == 1)
-    {
-        if (str_1[0] == str_2[0]) return true;
-        else return false;
-    }
 
-    for (int it_1 = 0; it_1 < str_1.length(); ++it_1)
+    // Iterate through indices of first string.
+    for (int it_1 = 0; it_1 < str_length; ++it_1)
     {
-        for (int it_2 = 0; it_2 < str_2.length(); ++it_2)
+        // Stop iterating through indices of first string when we find a
+        // character that matches the first character in the second string.
+        if (str_1[it_1] == str_2[0])
         {
-            if (str_1[it_1] == str_2[it_2])
+            int it_2;
+            int current_1 = it_1;
+
+            // Iterate through indices of second string.
+            for (it_2 = 0; it_2 < str_length; ++it_2)
             {
-                int start = it_2;
-                int current_1 = it_1 + 1;
-                int current_2 = start + 1;
+                // If we've hit the end of the first string, we need to loop back to the start.
+                if (current_1 == str_length) current_1 = 0;
 
-                while (current_2 != start)
-                {
-                    if (current_1 == str_length) current_1 = 0;
-                    if (current_2 == str_length) current_2 = 0;
-                    if (str_1[current_1] != str_2[current_2]) break;
-                    ++current_1;
-                    ++current_2;
-                }
+                // If a character in the first string doesn't match a character in the second string,
+                // these slices aren't equal, and we can break out of this loop.
+                if (str_1[current_1] != str_2[it_2]) break;
 
-                if (current_2 == start) return true;
+                ++current_1;
             }
+
+            // If we got through the entirety of the second string without breaking, then
+            // the two strings have at least one slice that is equal, so we can return true.
+            if (it_2 == str_length) return true;
         }
     }
 
